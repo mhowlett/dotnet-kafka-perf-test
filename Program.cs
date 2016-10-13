119a2b565edb8940d3b49014b38e23a45d8840c3
Matt Howlett <matt@confluent.io>
2016-10-13 14:37:13 -0700

    WaitAll versions

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using KafkaNet;
@@ -9,13 +10,14 @@ namespace ConsoleApplication
 {
     public class Program
     {
-        public static async Task _main()
+        public static int N = 50;
+
+        public static async Task RdKafka_Await()
         {
-            Console.WriteLine("Start");
             using (RdKafka.Producer producer = new RdKafka.Producer("127.0.0.1:9092"))
             using (Topic topic = producer.Topic("testtopic"))
             {
-                for (int i=0; i<100000; ++i)
+                for (int i=0; i<N; ++i)
                 {
                     byte[] data = Encoding.UTF8.GetBytes("Hello RdKafka");
                     DeliveryReport deliveryReport = await topic.Produce(data);
@@ -25,13 +27,30 @@ namespace ConsoleApplication
             Console.WriteLine("End");
         }
 
-        public static async Task _main2()
+        public static void RdKafka_WaitAll()
+        {
+            DateTime startTime = DateTime.Now;
+            using (RdKafka.Producer producer = new RdKafka.Producer("127.0.0.1:9092"))
+            using (Topic topic = producer.Topic("testtopic"))
+            {
+                Task<DeliveryReport>[] tasks = new Task<DeliveryReport>[N];
+                for (int i=0; i<N; ++i)
+                {
+                    byte[] data = Encoding.UTF8.GetBytes("Hello RdKafka");
+                    tasks[i] = topic.Produce(data);
+                }
+                Task.WaitAll(tasks);
+            }
+            Console.WriteLine("time to complete: " + (DateTime.Now - startTime).TotalMilliseconds.ToString());
+        }
+
+        public static async Task KafkaNet_Await()
         {
             var options = new KafkaOptions(new Uri("http://127.0.0.1:9092"));
             var router = new BrokerRouter(options);
             var client = new KafkaNet.Producer(router);
 
-            for (int i=0; i<100000; ++i)
+            for (int i=0; i<N; ++i)
             {
                 await client.SendMessageAsync("testtopic", new[] { new KafkaNet.Protocol.Message("hello world")});
             }
@@ -39,9 +58,41 @@ namespace ConsoleApplication
             using (client) { }
         }
 
+        public static void KafkaNet_WaitAll()
+        {
+            DateTime startTime = DateTime.Now;
+
+            var options = new KafkaOptions(new Uri("http://127.0.0.1:9092"));
+            var router = new BrokerRouter(options);
+            var client = new KafkaNet.Producer(router);
+
+            Task[] tasks = new Task[N];
+            for (int i=0; i<N; ++i)
+            {
+                tasks[i] = client.SendMessageAsync("testtopic", new[] { new KafkaNet.Protocol.Message("hello world")});
+            }
+
+            using (client)
+            {
+                Task.WaitAll(tasks);
+            }
+
+            Console.WriteLine("time to complete: " + (DateTime.Now - startTime).TotalMilliseconds.ToString());
+        }
+
         public static void Main(string[] args)
         {
-            _main().Wait();
+            // KafkaNet_Await.Wait();
+            // RdKafka_Await.Wait();
+
+            //KafkaNet_WaitAll();
+            RdKafka_WaitAll();
+
+            // Results on my mac book pro (ms):
+            // N          RdKafka    KafkaNet
+            // 50          ~1200         ~250
+            // 500000      ~1800        ~7200
+            // 5000000    ~10000       ~68000
         }
     }
 }
